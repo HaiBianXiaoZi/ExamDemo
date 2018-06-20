2fdc479458e39eba794e8399dd0de2b0c5d461cd
HaiBianXiaoZi <40025517+HaiBianXiaoZi@users.noreply.github.com>
2018-06-20 21:36:50 +0800

    task类增加消耗

--- src/main/java/com/migu/schedule/info/TaskInfo.java
+++ src/main/java/com/migu/schedule/info/TaskInfo.java
@@ -6,15 +6,18 @@ package com.migu.schedule.info;
  * @author
  * @version
  */
-public class TaskInfo
+public class TaskInfo implements Comparable<TaskInfo>
 {
     private int taskId;
     private int nodeId;
+    private int consumption;
     public int getNodeId()
     {
         return nodeId;
     }
     public int getTaskId(){  return taskId; }
+    
+    public int getConsumption(){  return consumption; }
     public void setNodeId(int nodeId)
     {
         this.nodeId = nodeId;
@@ -23,9 +26,20 @@ public class TaskInfo
     {
         this.taskId = taskId;
     }
+    public void setConsumption(int consumption)
+    {
+        this.consumption = consumption;
+    }
+    
     @Override
     public String toString()
     {
-        return "TaskInfo [taskId=" + taskId + ", nodeId=" + nodeId + "]";
+        return "TaskInfo [taskId=" + taskId + ", nodeId=" + nodeId + ", consumption=" + consumption + "]";
     }
+	public int compareTo(TaskInfo o) {
+		
+		if(taskId >o.taskId) return 1;
+		if(taskId < o.taskId) return -1;
+		return 0;
+	}
 }
